c49a5277ef91dfbf34fb2b9a0b43ec7ea1452cbc
Kevin Wong <kevin.wong1@gmail.com>
2019-01-01 21:41:23 -0500

    (v4) nested routes, add new comments and create routes, add new comment form and button

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require("express"),
 	app = express(),
 	bodyParser = require("body-parser"),
 	mongoose = require("mongoose"),
-	Campground = require("./models/campground")
+	Campground = require("./models/campground"),
+	Comment = require("./models/comment"),
 	seedDB = require("./seeds")
 
 
@@ -69,9 +70,38 @@ app.get("/campgrounds/:id", function(req, res){
 //===============================
 
 app.get("/campgrounds/:id/comments/new", function(req, res){
-  res.render("comments/new")
-});
+	// find campground by id
+	Campground.findById(req.params.id, function(err, campground){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("comments/new", {campground: campground});
+		}
+	});
+  });
 
+app.post("/campgrounds/:id/comments", function(req, res){
+	// lookup campground using ID
+	Campground.findById(req.params.id, function(err, campground){
+	  if(err){
+	  	console.log(err);
+	  	res.redirect("/campgrounds");
+	  } else {
+	  	Comment.create(req.body.comment, function(err, comment){
+	  		if(err){
+	  			console.log(err);
+	  		} else {
+	  			campground.comments.push(comment);
+	  			campground.save();
+	  			res.redirect('/campgrounds/' + campground._id);
+	  		}
+	  	});
+	  }
+	});
+	// create new comment
+	// connect new comment to campground
+	// redirect campground show page
+})
 app.listen(3000, process.env.IP, function(){
 	console.log("YelpCamp Server has Started");
 })
