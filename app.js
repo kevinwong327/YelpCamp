440e753bd6393efbdce58ecfd9eafec540b892a6
Kevin Wong <kevin.wong1@gmail.com>
2019-03-26 02:45:18 -0400

    (v18) Password Reset

--- app.js
+++ app.js
@@ -1,40 +1,46 @@
 //require('dotenv').config();
 
-var express = require("express"),
-app = express(),
-bodyParser = require("body-parser"),
-mongoose = require("mongoose"),
-flash = require("connect-flash"),
-passport = require("passport"),
-LocalStrategy = require("passport-local"),
-methodOverride = require("method-override"),
-Campground = require("./models/campground"),
-Comment = require("./models/comment"),
-User = require("./models/user"),
-seedDB = require("./seeds")
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	flash = require('connect-flash'),
+	passport = require('passport'),
+	cookieParser = require('cookie-parser'),
+	LocalStrategy = require('passport-local'),
+	methodOverride = require('method-override'),
+	Campground = require('./models/campground'),
+	Comment = require('./models/comment'),
+	User = require('./models/user'),
+	seedDB = require('./seeds');
 
 //requiring routes
-var commentRoutes = require("./routes/comments"),
-	campgroundRoutes = require("./routes/campgrounds"),
-	indexRoutes =  require("./routes/index")
+var commentRoutes = require('./routes/comments'),
+	campgroundRoutes = require('./routes/campgrounds'),
+	indexRoutes = require('./routes/index');
 
+mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
+app.use(cookieParser('secret'));
 
-mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true});
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
+//require moment
+app.locals.moment = require('moment');
+
 app.use(flash());
 
 // seedDB(); // seed the database
 
-
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
-	secret: "Once again I got the ticket to score",
-	resave: false,
-	saveUninitialized: false
-}));
+app.use(
+	require('express-session')({
+		secret: 'Once again I got the ticket to score',
+		resave: false,
+		saveUninitialized: false
+	})
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -42,18 +48,17 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // add middleware to make currentUser available to every single route
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
 	res.locals.currentUser = req.user;
-	res.locals.error = req.flash("error");
-	res.locals.success =req.flash("success");
+	res.locals.success = req.flash('success');
+	res.locals.error = req.flash('error');
 	next();
 });
 
-app.use("/", indexRoutes);
-app.use("/campgrounds", campgroundRoutes);
-app.use("/campgrounds/:id/comments", commentRoutes);
+app.use('/', indexRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/comments', commentRoutes);
 
-
-app.listen(3000, process.env.IP, function(){
-	console.log("YelpCamp Server has Started");
-})
+app.listen(3000, process.env.IP, function() {
+	console.log('YelpCamp Server has Started');
+});
--- models/user.js
+++ models/user.js
@@ -1,14 +1,18 @@
-var mongoose = require("mongoose");
-var passportLocalMongoose = require("passport-local-mongoose");
+var mongoose = require('mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
 var UserSchema = new mongoose.Schema({
-	username: String,
+	username: { type: String, unique: true, required: true },
 	password: String,
 	avatar: String,
 	firstName: String,
 	lastName: String,
-	email: String,
-	isAdmin: {type: Boolean, default: false}
+	email: { type: String, unique: true, required: true },
+	resetPasswordToken: String,
+	resetPasswordExpires: Date,
+	isAdmin: { type: Boolean, default: false }
 });
 
 UserSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema);
